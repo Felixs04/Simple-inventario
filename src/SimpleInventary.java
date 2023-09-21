1f4c112879849ef1ee12380e9f6349bce02574d7
Felix Suriel lugo <felixsuriellugo@Felixs-MacBook-Air.local>
2023-09-21 19:21:45 -0400

    nueva clase agregada

--- src/SimpleInventary.java
+++ src/SimpleInventary.java
@@ -1,2 +1,14 @@
 public class SimpleInventary {
+    public static void agregararticulo(String articulo) {
+    }
+    public static void removerarticulo(int id) {
+    }
+    public static double obtenrprecioarticulo(int id) {
+        return 0.00;
+    }
+    public static void imprimirinventario(){
+    }
+    public static void main(String[] args) {
+        System.out.println("Ingresar una opcion");
+    }
 }
